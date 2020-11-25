fe67b91838d86b0a3982bf5f13c480f92e185832
Ben <benpaullamb@gmail.com>
2020-11-25 14:06:44 +0000

    Created foundation

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+    render() {
+        return (
+            <div>Igloo</div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const output = path.join(__dirname, './public');
+
+module.exports = {
+    entry: './src/App.js',
+    output: {
+        path: output,
+        filename: 'bundle.js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    devServer: {
+        contentBase: output
+    }
+};
