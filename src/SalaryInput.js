2c3fa0c3ff7e871d095dc91b7dc1f71f7f91c7aa
Ben <benpaullamb@gmail.com>
2020-11-25 16:09:01 +0000

    Added national insurance and student loan

--- src/SalaryInput.js
+++ src/SalaryInput.js
@@ -12,10 +12,10 @@ export default class SalaryInput extends Component {
         return (
             <div className="input page">
                 <div className="page__container">
-                    <span className="input__prompt">Calulate your income tax</span>
+                    <span className="input__prompt">Calulate your yearly income tax</span>
                     <input
                         value={this.state.grossSalary} onChange={e => this.onSalaryChange(e.target.value)}
-                        type="number" placeholder="Enter gross salary" className="input__input"/>
+                        type="number" placeholder="Enter yearly gross salary" className="input__input"/>
                     <button onClick={() => this.onCalculate()} className="input__button">Calculate</button>
                 </div>
             </div>
--- src/TaxResults.js
+++ src/TaxResults.js
@@ -25,19 +25,49 @@ export default class TaxResults extends Component {
 
         // Additional rate
         if(grossSalary > 150000) {
-            higherRate = (grossSalary - 150000) * 0.45;
+            additionalRate = (grossSalary - 150000) * 0.45;
         }
 
+        const nationalInsurance = this.calculateNI(grossSalary);
+        const monthlyStudentLoan = this.calculateStudentLoan(grossSalary);
         const totalTax = basicRate + higherRate + additionalRate;
-        const netSalary = grossSalary - totalTax;
+        const netSalary = grossSalary - nationalInsurance - totalTax - (monthlyStudentLoan * 12);
 
         return {
             basicRate: basicRate.toFixed(2),
             higherRate: higherRate.toFixed(2),
             additionalRate: additionalRate.toFixed(2),
+            nationalInsurance: nationalInsurance.toFixed(2),
+            monthlyStudentLoan: monthlyStudentLoan.toFixed(2),
+            yearlyStudentLoan: (monthlyStudentLoan * 12).toFixed(2),
             totalTax: totalTax.toFixed(2),
             netSalary: netSalary.toFixed(2)
         };
+    }
+
+    calculateNI(grossSalary) {
+        let nationalInsurance = 0;
+        const monthlySalary = grossSalary / 12;
+        const niPrimaryThreshold = 792; // per month
+        const niUpperEarningsLimit = 4167; // per month
+
+        if(monthlySalary > niPrimaryThreshold) {
+            if(monthlySalary <= niUpperEarningsLimit) {
+                nationalInsurance = (monthlySalary - niPrimaryThreshold) * 0.12 * 12;
+            } else {
+                nationalInsurance = (niUpperEarningsLimit - niPrimaryThreshold) * 0.12 * 12;
+                nationalInsurance += (monthlySalary - niUpperEarningsLimit) * 0.02 * 12;
+            }
+        }
+
+        return nationalInsurance;
+    }
+
+    // Plan 1
+    calculateStudentLoan(grossSalary) {
+        const monthlySalary = grossSalary / 12;
+        const threshold = 1615;
+        return monthlySalary > threshold ? (monthlySalary - threshold) * 0.09 : 0;
     }
 
     render() {
@@ -50,7 +80,7 @@ export default class TaxResults extends Component {
                         <thead>
                             <tr>
                                 <th className="results__band">Band</th>
-                                <th>Amount Taxed</th>
+                                <th>Annual Amount</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -71,13 +101,22 @@ export default class TaxResults extends Component {
                                 <td>£{taxes.additionalRate}</td>
                             </tr>
                             <tr>
-                                <td className="results__total">Total Tax</td>
+                                <td className="results__special">National Insurance</td>
+                                <td>£{taxes.nationalInsurance}</td>
+                            </tr>
+                            <tr>
+                                <td className="results__special">Student Loan</td>
+                                <td>£{taxes.yearlyStudentLoan}</td>
+                            </tr>
+                            <tr>
+                                <td className="results__special">Total Tax</td>
                                 <td>£{taxes.totalTax}</td>
                             </tr>
                         </tbody>
                     </table>
 
                     <span className="results__net">Net Salary: £{taxes.netSalary}</span>
+                    <span className="results__student">Monthly Student Loan: £{taxes.monthlyStudentLoan}</span>
                 </div>
             </div>
         );
